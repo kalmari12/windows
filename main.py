6405e35800dc02aa8834a50802a29c07f563e670
kalmari12 <andrisszb23@gmail.com>
2026-06-16 13:07:35 +0200

    Clicker base

---
+++ main.py
@@ -0,0 +1,107 @@
+import pygame
+import pygame_gui
+
+###BACKGROUND SHI###
+pygame.init()
+
+pygame.display.set_caption('Windows')
+window_surface = pygame.display.set_mode((800, 600))
+
+### Háttérszín és inicializálás
+background = pygame.Surface((800, 600))
+background.fill(pygame.Color("#27303B"))
+
+### Alap változók a játékhoz mint pl.: clicks
+manager = pygame_gui.UIManager((800, 600))
+clicks = 0
+rect = pygame.Rect(100, 100, 300, 200)
+
+###---Gens
+Gen_amount = 0
+Gen_price = 0
+
+###---Power
+Power = 1
+Power_Upgrade_Price = 0
+
+### Clicker rész ikonjának a kreálása
+Clicker_Icon = pygame_gui.elements.UIButton(
+    relative_rect=pygame.Rect(50, 50,70,50),
+    text="Clicker",
+    manager=manager,
+)
+### Shop ikonjának a kreálása
+Shop_Icon = pygame_gui.elements.UIButton(
+    relative_rect=pygame.Rect(150, 50,70,50),
+    text="Shop",
+    manager=manager,
+)
+
+### Emiatt nem mutat semmit alapbol a kis windowwal kapcsolatban
+my_window = None
+Clicks_text = None
+ggonb = None
+
+clock = pygame.time.Clock()
+is_running = True
+
+#########
+###FUGGVENYEK###
+def open_clicker_window():
+    global my_window, Clicks_text, ggonb
+
+    my_window = pygame_gui.elements.UIWindow(
+    rect=rect,
+    manager=manager,
+    window_display_title="Clicker",
+    object_id='#Click_Window'
+    )
+
+    ggonb = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect(0,0,150,30),
+        text="Click me",
+        manager=manager,
+        container=my_window,
+        anchors={"center":"center"}
+    )
+
+    Clicks_text = pygame_gui.elements.UILabel(
+        relative_rect=pygame.Rect(0,-50,100,50),
+        text=f"Clicks: {clicks}",
+        manager=manager,
+        container=my_window,
+        anchors={"center":"center"}
+        )
+
+#########
+###MAIN###
+while is_running:
+    time_delta = clock.tick(60)/1000.0
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_running = False
+        ### Ablak bezárása
+        if event.type == pygame_gui.UI_WINDOW_CLOSE:
+            if event.ui_element == my_window:
+                my_window = None
+        ### Ablak nyitása
+        if event.type == pygame_gui.UI_BUTTON_PRESSED:
+            if event.ui_element == Clicker_Icon:
+                if my_window is None:
+                    open_clicker_window()
+        ### Clickek regisztralasa es tarolasa
+        if event.type == pygame_gui.UI_BUTTON_PRESSED:
+            if event.ui_element == ggonb:
+                clicks += 1
+                print(clicks)
+                Clicks_text.set_text(f"Clicks: {clicks}")
+
+
+        manager.process_events(event)
+
+    manager.update(time_delta)
+
+    window_surface.blit(background, (0, 0))
+    manager.draw_ui(window_surface)
+
+    pygame.display.update()
